84dbaf42654656a35ebfc2810d9e1d400956e870
Pabloroosario <61844746+Pabloroosario@users.noreply.github.com>
2022-05-05 13:50:47 +0200

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -7,11 +7,12 @@
 #define MAX_BUFFER 200
 #define PAUSA_MS 200 
 
+int Enviar_y_Recibir(Serial* Arduino, const char* mensaje_enviar, char* mensaje_recibir);
 
 int main(void) {
 
 	Serial* Arduino;
-	char puerto[] = "COM4"; // Puerto serie al que está conectado Arduino
+	char puerto[] = "COM4"; // Puerto serie al que estÃ¡ conectado Arduino
 	int opcion_menu;
 	setlocale(LC_ALL, "es-ES");
 	Arduino = new Serial((char*)puerto);
@@ -20,3 +21,30 @@ int main(void) {
 
 	return 0;
 }
+
+
+
+int Enviar_y_Recibir(Serial* Arduino, const char* mensaje_enviar, char* mensaje_recibir)
+{
+ int bytes_recibidos=0,total=0;
+ int intentos = 0,fin_linea=0;
+ Arduino->WriteData((char *)mensaje_enviar,strlen(mensaje_enviar));
+ Sleep(PAUSA_MS);
+ bytes_recibidos = Arduino->ReadData(mensaje_recibir,sizeof(char)*MAX_BUFFER-1);
+ while ((bytes_recibidos > 0 || intentos < 5) && fin_linea==0)
+ {
+ if (bytes_recibidos > 0)
+ {
+ total += bytes_recibidos;
+ if (mensaje_recibir[total - 1] == 13 || mensaje_recibir[total - 1] == 10)
+ fin_linea = 1;
+ }
+ else
+ intentos++;
+ Sleep(PAUSA_MS);
+ bytes_recibidos = Arduino->ReadData(mensaje_recibir+total, sizeof(char)*MAX_BUFFER-1);
+ }
+ if (total > 0)
+ mensaje_recibir[total - 1] = '\0';
+ return total;
+} 
